9ca9ea145571d7b462b9d0ee3921b14b9dbcabc3
wangbingnan136 <42114835+wangbingnan136@users.noreply.github.com>
2022-03-06 23:53:45 +0800

    Create seed_everything.py

---
+++ seed_everything.py
@@ -0,0 +1,16 @@
+def seed_everything(seed=0):
+    import random
+    import numpy as np
+    import os
+    import torch
+    import tensorflow as tf
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+seed_everything(12345)
